6a9e8c32cbb51ff12e7ed8e6043b5ffcf4b3e090
Joy-singha <joys1682@gmail.com>
2021-05-17 17:22:23 +0600

    {project}

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
+
+    //stiky manu
+
+    $(".js--services-section").waypoint(function(direction){
+        if(direction=="down"){
+            $("nav").addClass("sticky");
+        }else{
+            $("nav").removeClass("sticky");
+        }
+    });
+
+
+
+
+
+
+
+
+    // mixitup (portfolio)
     var mixer= mixitup('.container');
 }) ;
